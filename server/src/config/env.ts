00b70b84c12b9008c2165e3d06b1b2ba063152e4
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2026-03-07 23:12:37 +0100

    CS-2 Add Local Docker Setup (Dockerfile + Docker Compose)

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,8 +1,17 @@
-import * as z from 'zod';
+import * as z from "zod";
 
 const EnvSchema = z.object({
-  NODE_ENV: z.string().default('development'),
-  PORT: z.string().default('3000'),
+  NODE_ENV: z.string().default("development"),
+  SERVER_PORT: z.coerce.number().default(5432),
+
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.coerce.number().default(5432),
+
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.coerce.number().default(5432),
 });
 
 const env = EnvSchema.parse(process.env);
