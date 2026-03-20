c50a0582c64022d3ab2409f03c031d214b1ddbb6
Bogdan Milojević <bogi.milojevic@gmail.com>
2026-03-20 16:38:04 +0100

    Rename DB file and add VSCode import ignore

--- server/src/data/dbClient.ts
+++ server/src/data/dbClient.ts
@@ -1,6 +1,8 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import env from '../config/env.ts';
 
+// TODO: Move to config folder? Also add drizzle config for drizzle kit to config folder
+
 type DbClient = ReturnType<typeof getDbClient>;
 
 const DATABASE_URL = `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`;
