c4056f10bf06b8fcb250d3537cfc3e367996b355
Bogdan Milojević <bogi.milojevic@gmail.com>
2026-05-11 22:51:28 +0200

    Add unique constraint to job_application

--- server/src/data/schema/job-application.schema.ts
+++ server/src/data/schema/job-application.schema.ts
@@ -1,17 +1,25 @@
-import { boolean, integer, pgTable, text } from 'drizzle-orm/pg-core';
+import { boolean, integer, pgTable, text, unique } from 'drizzle-orm/pg-core';
 import { timestamps } from '../util/utils.ts';
 import { jobPosting } from './job-posting.schema.ts';
 
 import { user } from './auth.schema.ts';
 
-export const jobApplication = pgTable('job_application', {
-  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-  userId: text('user_id').references(() => user.id),
-  jobPostingId: integer('job_posting_id').references(() => jobPosting.id),
-  status: text('status').notNull(),
-  isDeleted: boolean('is_deleted').default(false).notNull(),
-  ...timestamps,
-});
+export const jobApplication = pgTable(
+  'job_application',
+  {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    userId: text('user_id')
+      .references(() => user.id)
+      .notNull(),
+    jobPostingId: integer('job_posting_id')
+      .references(() => jobPosting.id)
+      .notNull(),
+    status: text('status').notNull(),
+    isDeleted: boolean('is_deleted').default(false).notNull(),
+    ...timestamps,
+  },
+  (table) => [unique('user_id_job_posting_id_unq').on(table.userId, table.jobPostingId)],
+);
 
 export type JobApplication = typeof jobApplication.$inferSelect;
 export type JobApplicationInsert = typeof jobApplication.$inferInsert;
