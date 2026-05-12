4ef7b6e0aae1e044a4a115faabeaef180409bec0
Bogdan Milojević <bogi.milojevic@gmail.com>
2026-05-12 09:52:56 +0200

    Add transactional insertWithStatusHistory

--- server/src/data/repositories/job-application.repository.ts
+++ server/src/data/repositories/job-application.repository.ts
@@ -3,10 +3,24 @@ import { jobApplication, JobApplication, JobApplicationInsert } from '../schema/
 import { inject, injectable } from 'tsyringe';
 import { TOKENS } from '../../config/dependency-tokens.ts';
 import { DbClient } from '../../config/db-client.ts';
+import { applicationStatusHistory } from '../schema/application-status-history.schema.ts';
 
 @injectable()
 export class JobApplicationRepository extends GenericRepository<JobApplication, JobApplicationInsert, number> {
   constructor(@inject(TOKENS.db) db: DbClient) {
     super(db, jobApplication);
+  }
+
+  async insertWithStatusHistory(payload: JobApplicationInsert): Promise<JobApplication> {
+    return await this.db.transaction(async (tx) => {
+      const [createdJobApplication] = await tx.insert(jobApplication).values(payload).returning();
+
+      await tx.insert(applicationStatusHistory).values({
+        jobApplicationId: createdJobApplication.id,
+        status: createdJobApplication.status,
+      });
+
+      return createdJobApplication;
+    });
   }
 }
